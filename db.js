127173ba859c4277ed7f7e1eabe82284683cdf25
Jungro-hub <59046575+Jungro-hub@users.noreply.github.com>
2020-02-25 15:02:02 +0900

    Controller part finished!

---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+  {
+    id: 8282,
+    title: "Video 8282",
+    description: "This is db test",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 11111,
+      name: "junro",
+      email: "jungro@gmail.com"
+    }
+  },
+  {
+    id: 222,
+    title: "Video 222",
+    description: "This is db test222",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 11111,
+      name: "junro",
+      email: "jungro@gmail.com"
+    }
+  },
+  {
+    id: 555,
+    title: "Video 555",
+    description: "This is db test555",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 11111,
+      name: "junro",
+      email: "jungro@gmail.com"
+    }
+  }
+];
