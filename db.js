6494471eed05691c3963566fa2d75b13770d5d0f
Jungro-hub <59046575+Jungro-hub@users.noreply.github.com>
2020-02-25 16:04:21 +0900

    Git CLI test

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ mongoose.connect("mongodb://localhost:27017/wetube", {
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅ Connected to DB");
+const handleOpen = () => console.log("✅ Connected to DB!");
 const handleError = error => console.log(`Error on DB Connection: ${error}`);
 
 db.once("open", handleOpen);
