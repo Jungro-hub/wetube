ece8c44bb5661b75aca3610fcf03d0948bccbb7d
Jungro-hub <59046575+Jungro-hub@users.noreply.github.com>
2020-02-25 15:56:41 +0900

    Connetted to mongoDB

--- db.js
+++ db.js
@@ -1,41 +1,14 @@
-export const videos = [
-  {
-    id: 8282,
-    title: "Video 8282",
-    description: "This is db test",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 11111,
-      name: "junro",
-      email: "jungro@gmail.com"
-    }
-  },
-  {
-    id: 222,
-    title: "Video 222",
-    description: "This is db test222",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 11111,
-      name: "junro",
-      email: "jungro@gmail.com"
-    }
-  },
-  {
-    id: 555,
-    title: "Video 555",
-    description: "This is db test555",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 11111,
-      name: "junro",
-      email: "jungro@gmail.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/wetube", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log(`Error on DB Connection: ${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
