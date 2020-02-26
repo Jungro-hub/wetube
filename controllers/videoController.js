69202cd9f8ecaa65a25b9bf0c8c8c09159fa0d90
Jungro-hub <59046575+Jungro-hub@users.noreply.github.com>
2020-02-26 09:51:35 +0900

    video upload part finished. database create and uploading video logic to use multer middleware. and video link(src) edit from express.static.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,14 @@
 import routes from "../routes";
+import video from "../model/video";
 
-export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch {
+    console.log("Error from Video Database");
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -15,11 +22,17 @@ export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description }
+    body: { title, description },
+    file: { path }
   } = req;
-  res.redirect(routes.videoDetail(8282));
+  const newVideo = await video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
